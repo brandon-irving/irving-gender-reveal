9c2f86245b86a4f0deb71e9498a09a0fe6af7010
brandon-irving <birvingfau@gmail.com>
2024-10-09 05:28:46 +0900

    added layout and suspense

--- app/rsvp/page.tsx
+++ app/rsvp/page.tsx
@@ -1,5 +1,11 @@
 import RSVP from "@/components/rsvp";
+import Layout from "@/components/ui/layout";
+import { Suspense } from "react";
 
 export default function Page() {
-  return <RSVP />;
+  return (
+    <Suspense fallback={<Layout>Loading...</Layout>}>
+      <RSVP />
+    </Suspense>
+  );
 }
